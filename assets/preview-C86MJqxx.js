da304dbbdf38d6930f1ebcbda0364f34b77d199e
ItaliaNorway <tre.d.tre@gmail.com>
2025-04-10 13:59:40 +0200

    Updates

---
+++ assets/preview-C86MJqxx.js
@@ -0,0 +1,1 @@
+const e={parameters:{permissions:{"interest-cohort":"() => false"},actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}}};export{e as default};
